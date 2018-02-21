eb477c4789c3626a915d5fe662793b22d02fe2bf
Iegor Maiorov <maiorov.prog@gmail.com>
2018-02-21 21:43:45 +0200

    * added swagger ui

--- src/main/java/org/home/quickpoll/QuickPollApplication.java
+++ src/main/java/org/home/quickpoll/QuickPollApplication.java
@@ -1,10 +1,12 @@
 package org.home.quickpoll;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableSwagger
 public class QuickPollApplication {
 
 	public static void main(String[] args) {
