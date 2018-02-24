f97de64bdce13558d9e7ae25709750c39372e5a4
Iegor Maiorov <maiorov.prog@gmail.com>
2018-02-24 18:03:40 +0200

    * added api description to controllers

--- src/main/java/org/home/quickpoll/controller/advice/RestExceptionHandler.java
+++ src/main/java/org/home/quickpoll/controller/advice/RestExceptionHandler.java
@@ -25,7 +25,6 @@ import java.util.stream.Collectors;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
 
-//todo: look at controllerAdvice
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
