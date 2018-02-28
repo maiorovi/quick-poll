dc517267b78284440bdeaae80b0623f6793396a1
Iegor Maiorov <maiorov.prog@gmail.com>
2018-02-28 23:56:09 +0200

    * fixed compute result issue * create  v3 of api

---
+++ src/main/java/org/home/quickpoll/controller/v3/VoteController.java
@@ -0,0 +1,50 @@
+package org.home.quickpoll.controller.v3;
+
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.home.quickpoll.domain.Vote;
+import org.home.quickpoll.domain.mapper.VoteMapper;
+import org.home.quickpoll.dto.VoteDto;
+import org.home.quickpoll.exception.ResourceNotFoundException;
+import org.home.quickpoll.service.VoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController("VoteControllerV3")
+@RequestMapping("/v3/polls/{pollId}/votes")
+public class VoteController {
+
+    private VoteService voteService;
+    private VoteMapper voteMapper;
+
+    public VoteController(@Autowired VoteService voteService, @Autowired VoteMapper voteMapper) {
+        this.voteService = voteService;
+        this.voteMapper = voteMapper;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "creates a vote for some option of a poll", response = Void.class)
+    public ResponseEntity<?> createVote(@RequestBody VoteDto voteDto, @PathVariable("pollId") Long pollId) throws URISyntaxException {
+        final Vote createdVote = voteService.createVote(voteDto);
+
+        String str = String.format("/polls/%d/votes/%d",pollId, createdVote.getId());
+        final URI location = new URI(str);
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "creates a vote for some option of a poll", response = VoteDto.class, responseContainer = "List")
+    public ResponseEntity<?> getAllVotes(@PathVariable("pollId") Long pollId) {
+        if (!voteService.pollExists(pollId)) {
+            throw new ResourceNotFoundException("Can`t vote on a non existent poll");
+        }
+
+        return ResponseEntity.ok(voteService.getAllVotes(pollId));
+    }
+
+}
--- src/main/java/org/home/quickpoll/repository/VoteRepository.java
+++ src/main/java/org/home/quickpoll/repository/VoteRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
-    @Query(value = "select v.* from opt o, vote v where v.option_id = o.id and o.poll_id = ?1", nativeQuery = true)
+    @Query(value = "select v.* from opt o, vote v where v.option_id = o.option_id and o.poll_id = ?1", nativeQuery = true)
     List<Vote> findByPoll(Long pollId);
 }
